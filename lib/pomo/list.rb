8599aa23c89796640492cfc9d256d22a03d7390e
haochong <haochong@gmail.com>
2014-03-18 16:02:39 +0800

    add ENV['POMO_HOME'] for customizable home path in pomo

--- lib/pomo/list.rb
+++ lib/pomo/list.rb
@@ -20,7 +20,8 @@ module Pomo
       if opts[:init]
         path = '.pomo'
       else
-        path = (File.exists?('.pomo') && ENV['POMO_ENV']!='test') ? '.pomo' : File.join(ENV['HOME'],'.pomo')
+        pomo_home = ENV['POMO_HOME'] ? ENV['POMO_HOME'] : ENV['HOME']
+        path = (File.exists?('.pomo') && ENV['POMO_ENV']!='test') ? '.pomo' : File.join(pomo_home,'.pomo')
       end
       @path = File.expand_path path
       @tasks = []
